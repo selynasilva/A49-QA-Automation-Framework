4899124899d8d1687e764ed511c54e65990b706a
natfed2022 <natalia.fedchenko@fluentcommerce.com>
2023-08-17 19:54:56 +1000

    Added Login Tests for L16-Intro into Selenium

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -11,7 +11,6 @@ import java.time.Duration;
 public class LoginTests extends BaseTest {
     @Test
     public void loginEmptyEmailPassword() {
-
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
