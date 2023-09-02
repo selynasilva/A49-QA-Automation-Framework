3e796da326b55943354c5aec7660cd788f06f220
Natalia-Fedchenko <85151352+Natalia-Fedchenko@users.noreply.github.com>
2023-09-02 21:22:47 +1000

    Update BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -20,6 +21,7 @@ public class BasePage {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         actions = new Actions(driver);
+        PageFactory.initElements(driver,this);
     }
     public void navigateToPage(String url) {
         driver.get(url);
