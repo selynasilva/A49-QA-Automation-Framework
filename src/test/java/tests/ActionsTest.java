c1e21057427283bdd29a2cc9db24d4f71f6b3033
Natalia-Fedchenko <85151352+Natalia-Fedchenko@users.noreply.github.com>
2023-09-02 21:25:51 +1000

    Update ActionsTest.java

--- src/test/java/tests/ActionsTest.java
+++ src/test/java/tests/ActionsTest.java
@@ -4,30 +4,30 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
+import pages.SongsPage;
 
 public class ActionsTest extends BaseTest {
     LoginPage loginPage;
     HomePage homePage ;
+    SongsPage songsPage;
 
     @BeforeClass
     void login(){
         loginPage = new LoginPage(driver);
         homePage = new HomePage(driver);
+        songsPage = new SongsPage(driver);
         loginPage.loginCorrectCred();
     }
     @Test
     void contextClickOnSong()  {
         homePage.clickAllSongs();
-        homePage.contextClickSongByName("Reactor");
-        homePage.clickPlay();
+        songsPage.contextClickSongByName("Reactor").clickPlay();
         homePage.checkSongIsPlaying();
     }
 
     @Test
     void mouseHoverTest() {
-        homePage.clickAllSongs();
-        homePage.mouseMoveToPlayBtn();
-        homePage.checkIfPlayBtnIsVisible();
+        homePage.clickAllSongs().mouseMoveToPlayBtn().checkIfPlayBtnIsVisible();
     }
 //    COMMENTED OUT AS THAT PLAYLIST NEEDS TO BE CREATED BEFORE THE TEST
 //    @Test
