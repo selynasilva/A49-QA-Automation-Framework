469e77d6a852e92431148267f1ba1bc8f299d572
Natalia-Fedchenko <85151352+Natalia-Fedchenko@users.noreply.github.com>
2023-09-02 21:25:29 +1000

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,15 +1,25 @@
 package pages;
 
+import org.apache.commons.logging.Log;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver driver) {
         super(driver);
     }
+    @FindBy (css ="input[type='email']")
+    WebElement emailField;
+    @FindBy (css ="input[type='password']")
+    WebElement passwordField;
+    @FindBy (css ="[type='submit']")
+    WebElement submitBtn;
+
     public void loginCorrectCred() {
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
@@ -17,15 +27,20 @@ public class LoginPage extends BasePage {
     }
 
 
-    public void provideEmail(String email) {
-        enterText(By.cssSelector("input[type='email']"),email);
+    public LoginPage provideEmail(String email) {
+       wait.until(ExpectedConditions.elementToBeClickable(emailField)).clear();
+       emailField.sendKeys(email);
+       return this;
     }
 
-    public void providePassword(String password) {
-        enterText(By.cssSelector("input[type='password']"),password);
+    public LoginPage providePassword(String password) {
+        wait.until(ExpectedConditions.elementToBeClickable(passwordField)).clear();
+        passwordField.sendKeys(password);
+        return this;
     }
 
-    public void clickSubmit() {
-        clickOnElement(By.cssSelector("[type='submit']"));
+    public LoginPage clickSubmit() {
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+        return this;
     }
 }
