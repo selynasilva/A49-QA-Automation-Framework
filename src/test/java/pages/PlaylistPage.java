d6944df32b0e45ab9df71ece792ae487653c78c7
Natalia-Fedchenko <85151352+Natalia-Fedchenko@users.noreply.github.com>
2023-09-02 21:23:51 +1000

    Update PlaylistPage.java

--- src/test/java/pages/PlaylistPage.java
+++ src/test/java/pages/PlaylistPage.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.List;
@@ -16,7 +14,7 @@ public class PlaylistPage extends BasePage{
     public PlaylistPage(WebDriver driver) {
         super(driver);
     }
-    public void checkNumberOfSongsInPlaylist() {
+    public PlaylistPage checkNumberOfSongsInPlaylist() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".song-list-wrap.main-scroll-wrap.playlist td.title")));
         List<WebElement> allSongsInPlaylist =driver.findElements(By.cssSelector(".song-list-wrap.main-scroll-wrap.playlist td.title"));
         //This block is for visibility - printing all the songs
@@ -25,20 +23,23 @@ public class PlaylistPage extends BasePage{
             System.out.println(element.getText());
         }
         Assert.assertEquals(allSongsInPlaylist.size(),3);
+        return this;
     }
-    public void clickOnCreateNewPlaylist() {
+    public PlaylistPage clickOnCreateNewPlaylist() {
         wait.until(ExpectedConditions
                         .visibilityOfElementLocated(By.cssSelector("[data-testid='playlist-context-menu-create-simple']")))
                 .click();
+        return this;
     }
 
-    public void clickOnCreatePlaylistBtn() {
+    public PlaylistPage clickOnCreatePlaylistBtn() {
         wait.until(ExpectedConditions
                         .visibilityOfElementLocated(By.cssSelector("[data-testid='sidebar-create-playlist-btn']")))
                 .click();
+        return this;
     }
 
-    public void enterNewPlaylistName(String newPlayListName) {
+    public PlaylistPage enterNewPlaylistName(String newPlayListName) {
         WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
 //       clear() does not work, element has an attribute of "required"
 //       workaround is ctrl A (to select all) then backspace to clear then replace with new playlist name
@@ -47,24 +48,29 @@ public class PlaylistPage extends BasePage{
         playlistInputField.sendKeys(Keys.chord(Keys.COMMAND,"A", Keys.BACK_SPACE));
         playlistInputField.sendKeys(newPlayListName);
         playlistInputField.sendKeys(Keys.ENTER);
+        return this;
     }
 
-    public void clickOnDeletePlaylistBtn() {
+    public PlaylistPage clickOnDeletePlaylistBtn() {
         WebElement deletePlaylistBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".del.btn-delete-playlist")));
         actions.click(deletePlaylistBtn).perform();
+        return this;
     }
 
-    public void clickOnPlaylist(String playlistName) {
+    public PlaylistPage clickOnPlaylist(String playlistName) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'" + playlistName + "')]"))).click();
+        return this;
     }
 
-    public void doubleClickOnPlaylist(String playlistName) {
+    public PlaylistPage doubleClickOnPlaylist(String playlistName) {
         WebElement playlistElement =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'" + playlistName + "')]")));
         actions.doubleClick(playlistElement).perform();
+        return this;
     }
 
-    public void checkMessage(String playListName) {
+    public PlaylistPage checkMessage(String playListName) {
         WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Updated playlist')]")));
         Assert.assertEquals("Updated playlist \""+playListName+".\"",notification.getText());
+        return this;
     }
 }
