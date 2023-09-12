48f56bfda22cb294aa298ad0cc1f3a309cd9fbb4
Natalia-Fedchenko <85151352+Natalia-Fedchenko@users.noreply.github.com>
2023-09-12 15:24:24 +1000

    Update CucumberRunner.java

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -5,14 +5,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(
-        features = {"src/test/resources/features"},
-        glue = {"stepDefinition"},
-        plugin = {
-                "json:build/cucumber-reports/cucumber.json",
-                "rerun:build/cucumber-reports/rerun.txt"}
-)
-
     public class CucumberRunner extends AbstractTestNGCucumberTests {
         private TestNGCucumberRunner testNGCucumberRunner;
 
