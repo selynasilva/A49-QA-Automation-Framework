ce1595d3a32895d47da0be367f3c995abe69d910
Selyna <selyna.silva@testpro.io>
2023-09-27 23:37:10 -0700

    URL change
    Changed url to "https://tespro.io/"

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -19,6 +19,7 @@ public class LoginTests extends BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://testpro.io/";
+
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
